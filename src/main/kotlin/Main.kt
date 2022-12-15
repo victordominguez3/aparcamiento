db507467dd217d924cf1fcf34d4c845dc35d74f7
Víctor Domínguez <victor.dominguez@alumno.iesluisvives.org>
2022-12-15 11:01:47 +0100

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val aparcamiento = Array(4) { Array(4) {"Clase coche"} }
+    val coche = "Tiene matricula, antiguedad en base a matricula"
+    val
+
 }
