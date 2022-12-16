69687ebb643df800e3a791a909e5f9cf53872131
Víctor Domínguez <victor.dominguez@alumno.iesluisvives.org>
2022-12-16 13:32:21 +0100

    act

---
+++ src/main/kotlin/Coche.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+class Coche {
+
+    val ID = UUID.randomUUID()
+    val matricula = ""
+    var anyoFabricacion = 0
+
+    fun setMatricula() {
+
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
 fun main() {
 
-    val aparcamiento = Array(4) { Array(4) {"Clase coche"} }
+    val aparcamiento = Array(4) { Array(4) { Coche } }
     val coche = "Tiene matricula, antiguedad en base a matricula"
-    val
+//    aparcar -> si está lleno no
+//    salir
+//    donde aparcar? existe coche?
+//    cuantos coches hay?
+//    cuantos coches tiene un conductor en el parking?
+//    Listado de vehículos ordenados por antiguedad
+//    Recaudación
+//    Coche -> id, matricula, año
+//    Conductor -> id, nombre, dni
+//    Aparcar -> 3.75
 
 }
